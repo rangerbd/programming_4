9d3c620884717919649c2d99d540dc1afcdbe08b
rangerbd <dipto.g1@gmail.com>
2024-06-04 13:59:42 +0200

    adding part 1

--- assignment1_week1.py
+++ assignment1_week1.py
@@ -1,35 +1,67 @@
 
 
-class Atom:
-    def __init__(self,symbol,atomic_number, n_atom):
+class atom:
+    #1A
+    def __init__(self,symbol, atomic_number, number_of_neutrons):
         self.symbol = symbol
         self.atomic_number = atomic_number
-        self.number_of_neurons = n_atom
-
-    def isotope(self,new_neutrons):
-        self.number_of_neurons = new_neutrons
+        self.number_of_neutrons = number_of_neutrons
 
     def __repr__(self):
-        return f"Symbol = {self.symbol} , atomic_number = {self.atomic_number} and Number of atoms = {self.number_of_neurons} number of replaced neurons = {self.number_of_neurons}"
+        return f"Symbol is = {self.symbol}, atomic number is = {self.atomic_number}, number of neutrons are = {self.number_of_neutrons}"
+    
+    #1B
+    def proton_number(self):
+        return self.atomic_number
+    
+    def mass_number(self):
+        return self.atomic_number + self.number_of_neutrons
+    
+    #1C
 
-    def proton_number(n):
-        proton_number = n
-        return f"proton_number = {proton_number}"
+    def isotope(self, n_neutron):
+        self.number_of_neutrons = n_neutron 
+        return self.number_of_neutrons
 
-    def mass_number(proton_number,neutrons):
-        sum = proton_number + neutrons
-        return f"The Atoms mass number = {sum}"
 
-number_of_proton = Atom.proton_number(100)
-print(number_of_proton)
-sum_of_mass = Atom.mass_number(100,2)
-print(sum_of_mass) 
+    #1D
+    def equal(self,other):
+        if self.atomic_number != other.atomic_number:
+            raise ValueError("this is not comparable")
+        return self.mass_number() == other.mass_number()
+    def less_than(self,other):
+        if self.atomic_number != other.atomic_number:
+            raise ValueError("this is not comparable")
+        return self.mass_number() < other.mass_number()
+    
+    def greater_than(self, other):
+        if self.atomic_number != other.atomic_number:
+            raise ValueError("this is not comparable")
+        return self.mass_number() > other.mass_number()
 
-protium = Atom('H',1,0)
+protium = atom('H',6,2)
+a = atom ('H',5,6)
+b = atom('K',2,3)
 print(protium)
 
-protium.isotope(7)
-print(protium)
+print(f"proton number is {protium.proton_number()}  ")
+print(f"mass_number is {protium.mass_number()}")
+print(f"new assigned neutron number is {protium.isotope(5)}")
+
+#debug
+protium = atom('H', 1, 0)
+deuterium = atom('H', 1, 1)
+oxygen = atom('O', 8, 8)
+tritium = atom('H', 1, 2)
+oxygen.isotope(9)
+
+assert tritium.number_of_neutrons == 2
+assert tritium.mass_number() == 3
+assert protium.less_than(deuterium)
+assert deuterium.less_than(tritium) 
+assert tritium.greater_than(protium)
+
+
 
 
 
